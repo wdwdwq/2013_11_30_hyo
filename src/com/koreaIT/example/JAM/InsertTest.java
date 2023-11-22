578806b88d5bf10f3b8bd5f160a59e180efe0f7c
wdwdwq <qkenr7278@gamil.com>
2023-11-22 21:42:22 +0900

    JDBCInsertTest, DB.txt

---
+++ src/com/koreaIT/example/JAM/InsertTest.java
@@ -0,0 +1,50 @@
+package com.koreaIT.example.JAM;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
+
+public class InsertTest {
+	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+			conn = DriverManager.getConnection(url, "root", "");
+			String sql = "INSERT INTO article";
+			sql += " SET regDate = now()";
+			sql += ", updateDate = now()";
+			sql += ", title = '제목1'";
+			sql += ", `body` = '내용1'";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.executeUpdate();
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("에러: " + e);
+		} finally {
+			try {
+				if (pstmt != null && !pstmt.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
