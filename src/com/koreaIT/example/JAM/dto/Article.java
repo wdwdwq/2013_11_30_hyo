3b081d083f274e686295b1551121a6dba44c02f7
wdwdwq <qkenr7278@gamil.com>
2023-11-22 20:26:04 +0900

    JAM 시작

---
+++ src/com/koreaIT/example/JAM/dto/Article.java
@@ -0,0 +1,14 @@
+package com.koreaIT.example.JAM.dto;
+
+public class Article {
+	public int id;
+	public String title;
+	public String content;
+
+	public Article(int id, String title, String content) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+
+	}
+}
