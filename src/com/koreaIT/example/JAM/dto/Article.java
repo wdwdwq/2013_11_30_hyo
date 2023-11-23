81c68a47e4c16eb2449d96bbadaf5a26056686cd
wdwdwq <qkenr7278@gamil.com>
2023-11-23 20:33:59 +0900

    write,list

--- src/com/koreaIT/example/JAM/dto/Article.java
+++ src/com/koreaIT/example/JAM/dto/Article.java
@@ -1,14 +1,25 @@
 package com.koreaIT.example.JAM.dto;
 
 public class Article {
-	public int id;
-	public String title;
-	public String content;
 
-	public Article(int id, String title, String content) {
+	public int id;
+	public String regDate;
+	public String updateDate;
+	public String title;
+	public String body;
+	
+	public Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
-		this.content = content;
-
+		this.body = body;
 	}
+	
+	public Article(int id, String regDate, String updateDate, String title, String body) {
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.title = title;
+		this.body = body;
+	}
+	
 }
